214360c70776cbec170f4abfec21a048e2b26d1f
PvlMamaev <mpashtetp@yandex.ru>
2022-01-06 23:24:58 +0300

    Add LinearLayout vk screen
    added new screen and fixed something on yandex screen

--- app/src/main/java/com/example/firstloader/MainActivity.kt
+++ app/src/main/java/com/example/firstloader/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_yandex)
+        setContentView(R.layout.activity_linear_vk)
     }
     //something
 }
