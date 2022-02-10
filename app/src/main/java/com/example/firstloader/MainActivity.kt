7eb1b8f7867fab0101f208b83669bd4d7cae59bd
PvlMamaev <mpashtetp@yandex.ru>
2022-02-10 20:20:33 +0300

    calculator created
    калькулятор в сыром его виде без учета нулл, больших значений итп

--- app/src/main/java/com/example/firstloader/MainActivity.kt
+++ app/src/main/java/com/example/firstloader/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.firstloader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_vk)
+        setContentView(R.layout.calculator)
+
+        val edittext_first = findViewById<EditText>(R.id.edittext_first)
+        val edittext_second = findViewById<EditText>(R.id.edittext_second)
+        val textview_main = findViewById<TextView>(R.id.textview_main)
+        val button_main = findViewById<Button>(R.id.button_main)
+
+        button_main.setOnClickListener {
+            val sum =
+                edittext_first.text.toString().toInt() + edittext_second.text.toString().toInt()
+            textview_main.text = sum.toString()
+        }
     }
-    //something
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        val textview_main = findViewById<TextView>(R.id.textview_main)
+
+        outState.putString("KEY", textview_main.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+
+        val textview_main = findViewById<TextView>(R.id.textview_main)
+
+        textview_main.text = savedInstanceState.getString("KEY")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
 }
